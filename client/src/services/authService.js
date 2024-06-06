332dcad4b735489ad5eacd27d2236b797e99c956
CousemaAnjary <anjaryabdillah@gmail.com>
2024-06-06 15:03:47 +0300

    chore: Remove unused code and fix logout function in authService.js

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -39,14 +39,3 @@ export const logout = async () => {
         throw error.response ? error.response.data : new Error('Something went wrong during logout');
     }
 }
-
-export const isAuthenticated = () => {
-    const token = localStorage.getItem('userToken');
-    return !!token; // retourne true si le token existe, sinon false
-}
-
-export const removeToken = () => {
-    localStorage.removeItem('userToken');
-    localStorage.removeItem('firstName');
-    localStorage.removeItem('lastName');
-}
