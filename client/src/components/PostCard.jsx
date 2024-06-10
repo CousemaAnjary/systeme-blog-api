5cd894a9dd332b77c1109212b0c362ba6131e5c9
CousemaAnjary <anjaryabdillah@gmail.com>
2024-06-10 22:34:25 +0300

    chore: Update npm dependencies for date-fns to version 3.6.0

--- client/src/components/PostCard.jsx
+++ client/src/components/PostCard.jsx
@@ -1,53 +1,78 @@
+import { useState, useEffect } from "react";
 import { useNavigate } from 'react-router-dom';
 import { ThumbsUp, MessageSquare } from "lucide-react";
 import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
 import { Badge } from "@/components/ui/badge";
-import { Card, CardContent,CardDescription,CardFooter,CardHeader,CardTitle} from "@/components/ui/card";
- 
- 
-  
-  
-  
-  
+import { Card, CardContent, CardTitle, CardDescription } from "@/components/ui/card";
+import { getPublications } from "@/services/publicationService";
+import { formatDistanceToNow } from 'date-fns';
+import { fr } from 'date-fns/locale';
 
-
-const PostCard = ({ title, category, content, image, userImage, userName, likes, comments }) => {
+const PostCard = () => {
+    const [publications, setPublications] = useState([]);
     const navigate = useNavigate();
-    const handleClick = () => {
-        navigate(`/admin/showPost`);
+
+    useEffect(() => {
+        const fetchPublications = async () => {
+            try {
+                const data = await getPublications();
+                setPublications(data.publications);
+            } catch (error) {
+                console.error('Erreur lors de la récupération des publications:', error);
+            }
+        };
+
+        fetchPublications();
+    }, []);
+
+    const handleClick = (id) => {
+        navigate(`/admin/showPost/${id}`);
+    };
+
+    const truncateText = (text, maxLength) => {
+        if (text.length > maxLength) {
+            return text.substring(0, maxLength) + '...';
+        }
+        return text;
     };
 
     return (
-        <Card className="shadow-md">
-            {image && <img src={image} alt="Publication" className="rounded-t-lg w-full h-48 object-cover cursor-pointer" onClick={handleClick} />}
-            <CardContent>
-                <div className="flex items-center space-x-2 mb-2 mt-2">
-                    <Avatar>
-                        <AvatarImage src={userImage} alt="User Avatar" />
-                        <AvatarFallback>CN</AvatarFallback>
-                    </Avatar>
-                    <div className="text-lg font-semibold">{userName}</div>
-                </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            {publications.map((post, index) => (
+                <Card key={index} className="shadow-md">
+                    {post.image && <img src={`http://localhost:3000/${post.image}`} alt="Publication" className="rounded-t-lg w-full h-48 object-cover cursor-pointer" onClick={() => handleClick(post.id)} />}
+                    <CardContent>
+                        <div className="flex items-center space-x-2 mb-2 mt-2">
+                            <Avatar>
+                                <AvatarImage src={`http://localhost:3000/${post.user.image}`} alt="User Avatar" />
+                                <AvatarFallback>CN</AvatarFallback>
+                            </Avatar>
+                            <div className="flex flex-col">
+                                <div className="text-lg font-semibold">{post.user.firstname}</div>
+                                <div className="text-gray-500 text-sm">
+                                    {formatDistanceToNow(new Date(post.createdAt), { addSuffix: true, locale: fr })}
+                                </div>
+                            </div>
+                        </div>
 
-                <Badge className=" px-3 py-1 bg-pink-600 font-bold  mt-2 ">{category}</Badge>
-                <CardTitle className="mt-2">{title}</CardTitle>
-                <CardDescription className="leading-7 mt-2">{content}</CardDescription>
+                        <Badge className="px-3 py-1 bg-pink-600 font-bold mt-2">{post.category}</Badge>
+                        <CardTitle className="mt-2">{post.title}</CardTitle>
+                        <CardDescription className="leading-7 mt-2" dangerouslySetInnerHTML={{ __html: truncateText(post.content, 200) }} />
 
-                <div className="flex justify-between items-center mt-4">
-                    <button className="flex items-center space-x-1 text-gray-400">
-                        <ThumbsUp size={20} />
-                        <span>{likes} J’aime</span>
-                    </button>
-                    <div className="flex items-center space-x-1 text-gray-400">
-                        <MessageSquare size={20} />
-                        <span>{comments.length} commentaires</span>
-                    </div>
-                </div>
-            </CardContent>
-        </Card>
-
-
-
+                        <div className="flex justify-between items-center mt-4">
+                            <button className="flex items-center space-x-1 text-gray-400">
+                                <ThumbsUp size={20} />
+                                <span>45 J’aime</span>
+                            </button>
+                            <div className="flex items-center space-x-1 text-gray-400">
+                                <MessageSquare size={20} />
+                                <span>3 commentaires</span>
+                            </div>
+                        </div>
+                    </CardContent>
+                </Card>
+            ))}
+        </div>
     );
 };
 
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,52 +1,14 @@
-import { useState } from "react";
 import Navbar from "@/components/Navbar";
 import CreatePost from "@/components/CreatePost";
 import PostCard from "@/components/PostCard";
 
-const staticPosts = [
-    {
-        title: "Des super sushis que j’ai mangé dans le sud du Japon",
-        category: "Cuisine",
-        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit...",
-        image: "https://media-cdn.tripadvisor.com/media/photo-s/14/46/be/e7/sousi.jpg",
-        userImage: "https://img.freepik.com/psd-gratuit/personne-celebrant-son-orientation-sexuelle_23-2150115662.jpg",
-        userName: "John Doe",
-        comments: [
-            {
-                user: {
-                    avatar: "https://example.com/avatar1.jpg",
-                    name: "Jane Doe",
-                    timestamp: "Il y a 2 heures"
-                },
-                content: "Super publication !"
-            },
-            {
-                user: {
-                    avatar: "https://example.com/avatar2.jpg",
-                    name: "John Smith",
-                    timestamp: "Il y a 3 heures"
-                },
-                content: "J'adore cette photo."
-            }
-        ],
-        likes: 45
-    },
-    // Ajoutez plus de publications ici...
-];
-
 const Dashboard = () => {
-    const [posts] = useState(staticPosts);
-
     return (
         <div className="bg-gray-200 min-h-screen">
             <Navbar />
             <main className="container mx-auto p-16">
                 <CreatePost />
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-                    {posts.map((post, index) => (
-                        <PostCard key={index} {...post} />
-                    ))}
-                </div>
+                <PostCard />
             </main>
         </div>
     );
