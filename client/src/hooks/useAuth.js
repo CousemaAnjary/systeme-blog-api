5d21bbba87a2012c5c850908d39f626f93f8c4e1
CousemaAnjary <anjaryabdillah@gmail.com>
2024-06-13 17:11:25 +0300

    chore: Update login, registration, and authentication components and services
    This commit updates the login, registration, and authentication components and services to use a data object for form inputs. It also improves the code structure, error handling, and comments in the authService.js file. Additionally, the UserContext now initializes user data from localStorage. The API configuration and authentication services have been updated to include a JWT token in request headers. Unnecessary semicolons have been removed from the AuthenticatedSessionController.js file, unused code and imports have been removed from the RegisterForm component, and comment functionality has been added to the PostDetail component. The PostCard component has been updated to use a cyan background color for the category badge and adjust padding in the Dashboard page. The authentication controllers have been updated to handle missing fields and improve error messages. The RegisterForm component now uses capitalized comments and French translations.
    
    ```

--- client/src/hooks/useAuth.js
+++ client/src/hooks/useAuth.js
@@ -4,102 +4,140 @@ import { login as loginService, logout as logoutService, updateUser as updateUse
 import { isAuthenticated, removeToken } from '../utils/auth';
 import useUser from '../hooks/useUser';
 
-const useAuth = () => {
-    const [auth, setAuth] = useState(isAuthenticated());
-    const { user, setUser } = useUser();
-    const navigate = useNavigate();
 
+export default function useAuth() {
+    /**
+     * ! STATE (état, données) de l'application
+     */
+    const navigate = useNavigate()
+    const { user, setUser } = useUser()
+
+    const [auth, setAuth] = useState(isAuthenticated())
+
+    /**
+     * ! COMPORTEMENT (méthodes, fonctions) de l'application
+     */
     useEffect(() => {
+        // Vérifier si l'utilisateur est authentifié
         if (auth) {
+            // Charger les informations de l'utilisateur depuis le stockage local
             setUser({
                 firstname: localStorage.getItem('firstname'),
                 lastname: localStorage.getItem('lastname'),
                 email: localStorage.getItem('email'),
                 image: localStorage.getItem('image'),
-                coverPhoto: localStorage.getItem('coverPhoto'), // Charger la photo de couverture depuis le stockage local
-                userId: localStorage.getItem('userId') // Charger l'identifiant de l'utilisateur depuis le stockage local
+                coverPhoto: localStorage.getItem('coverPhoto'),
+                userId: localStorage.getItem('userId')
             });
         } else {
+            // Rediriger vers la page de connexion si l'utilisateur n'est pas authentifié
             navigate('/login');
         }
-    }, [auth, setUser, navigate]);
 
-    const login = async (email, password) => {
+    }, [auth, setUser, navigate]) // Déclencher l'effet uniquement si l'authentification, l'utilisateur ou la navigation change
+
+
+    const login = async (dataLogin) => {
         try {
-            const response = await loginService(email, password);
+            // Appeler le service d'authentification
+            const response = await loginService(dataLogin)
+
+            // Si la réponse de l'API contient un token JWT
             if (response.token) {
-                setAuth(true);
+                setAuth(true) // Authentifier l'utilisateur
+
+                // Stocker les informations de l'utilisateur dans le stockage local
                 setUser({
                     firstname: localStorage.getItem('firstname'),
                     lastname: localStorage.getItem('lastname'),
                     email: localStorage.getItem('email'),
                     image: localStorage.getItem('image'),
-                    coverPhoto: localStorage.getItem('coverPhoto'), // Charger la photo de couverture depuis le stockage local
-                    userId: localStorage.getItem('userId') // Charger l'identifiant de l'utilisateur depuis le stockage local
+                    coverPhoto: localStorage.getItem('coverPhoto'),
+                    userId: localStorage.getItem('userId')
                 });
             }
+
         } catch (error) {
-            console.error('Login failed:', error);
+            console.error('Login failed:', error)
             throw error;
         }
     };
 
     const updateUser = async (userData) => {
         try {
-            const response = await updateUserService(userData);
-            if (response) {
-                localStorage.setItem('firstname', userData.firstname);
-                localStorage.setItem('lastname', userData.lastname);
-                localStorage.setItem('email', userData.email);
-                // localStorage.setItem('userId', userData.id); // Mettre à jour l'identifiant de l'utilisateur
+            // Appeler le service de mise à jour de l'utilisateur
+            const response = await updateUserService(userData)
 
-                setUser(userData);
+            if (response) {
+                // Mettre à jour les informations de l'utilisateur dans le stockage local
+                localStorage.setItem('firstname', userData.firstname)
+                localStorage.setItem('lastname', userData.lastname)
+                localStorage.setItem('email', userData.email)
+
+                setUser(userData) // Mettre à jour les informations de l'utilisateur
             }
+
         } catch (error) {
-            console.error('Update user failed:', error);
+            console.error('Update user failed:', error)
             throw error;
         }
-    };
+    }
 
     const updateUserPhoto = async (formData) => {
         try {
-            const response = await updateUserPhotoService(formData);
+            // Appeler le service de mise à jour de la photo de profil
+            const response = await updateUserPhotoService(formData)
+
             if (response) {
-                localStorage.setItem('image', response.user.image);
-                setUser((prevUser) => ({ ...prevUser, image: response.user.image }));
+                // Mettre à jour la photo de profil de l'utilisateur dans le stockage local
+                localStorage.setItem('image', response.user.image)
+
+                // Mettre à jour la photo de profil de l'utilisateur
+                setUser((prevUser) => ({ ...prevUser, image: response.user.image }))
             }
+
         } catch (error) {
-            console.error('Update user photo failed:', error);
-            throw error;
+            console.error('Update user photo failed:', error)
+            throw error
         }
-    };
+    }
 
     const updateCoverPhoto = async (formData) => {
         try {
-            const response = await updateCoverPhotoService(formData);
+            // Appeler le service de mise à jour de la photo de couverture
+            const response = await updateCoverPhotoService(formData)
+
             if (response) {
-                localStorage.setItem('coverPhoto', response.user.coverPhoto);
-                setUser((prevUser) => ({ ...prevUser, coverPhoto: response.user.coverPhoto }));
+                // Mettre à jour la photo de couverture de l'utilisateur dans le stockage local
+                localStorage.setItem('coverPhoto', response.user.coverPhoto)
+
+                // Mettre à jour la photo de couverture de l'utilisateur
+                setUser((prevUser) => ({ ...prevUser, coverPhoto: response.user.coverPhoto }))
             }
+
         } catch (error) {
-            console.error('Update cover photo failed:', error);
-            throw error;
+            console.error('Update cover photo failed:', error)
+            throw error
         }
-    };
+    }
 
     const logout = async () => {
         try {
-            await logoutService();
-            removeToken();
-            setAuth(false);
-            setUser({ firstname: '', lastname: '', email: '', image: '', coverPhoto: '', userId: '' });
-            navigate('/login');
+            // Appeler le service de déconnexion
+            await logoutService()
+            removeToken() // Supprimer le token du stockage local
+            setAuth(false) // Déconnecter l'utilisateur
+            setUser({ firstname: '', lastname: '', email: '', image: '', coverPhoto: '', userId: '' }) // Réinitialiser les informations de l'utilisateur
+            navigate('/login') // Rediriger vers la page de connexion
         } catch (error) {
-            console.error('Logout failed:', error);
-            throw error;
+            console.error('Logout failed:', error)
+            throw error
         }
-    };
+    }
 
+    /**
+     * ! AFFICHAGE (render) de l'application
+     */
     return {
         isAuthenticated: auth,
         user,
@@ -108,7 +146,6 @@ const useAuth = () => {
         updateUser,
         updateUserPhoto,
         updateCoverPhoto
-    };
-};
+    }
 
-export default useAuth;
+}
