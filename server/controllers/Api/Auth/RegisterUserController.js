e65c040bf0b21dab6c14c13fd26ff7f507ae9df6
CousemaAnjary <anjaryabdillah@gmail.com>
2024-06-18 11:07:47 +0300

    chore: Mettre à jour le contrôleur RegisterUserController pour améliorer la gestion de l'enregistrement des utilisateurs

--- server/controllers/Api/Auth/RegisterUserController.js
+++ server/controllers/Api/Auth/RegisterUserController.js
@@ -3,17 +3,17 @@ const { User } = require('../../../models');
 module.exports = {
     async store(req, res) {
         // Récupération des données de la requête
-        const { email, password, lastname, firstname } = req.body;
+        const { email, password, lastname, firstname } = req.body
 
         // Vérification de la présence de l'email, du mot de passe, du nom et du prénom
         if (!email || !password || !lastname || !firstname) {
-            return res.status(400).json({ error: "Tous les champs sont obligatoires" });
+            return res.status(400).json({ error: "Tous les champs sont obligatoires" })
         }
 
         // Vérification si l'utilisateur existe déjà
-        const existingUser = await User.findOne({ where: { email } });
+        const existingUser = await User.findOne({ where: { email } })
         if (existingUser) {
-            return res.status(400).json({ error: "Email déjà utilisé" });
+            return res.status(400).json({ error: "Email déjà utilisé" })
         }
 
         try {
@@ -26,7 +26,7 @@ module.exports = {
             });
 
             // Sauvegarde de l'utilisateur dans la base de données
-            await user.save();
+            await user.save()
 
             // Réponse réussie avec les informations de l'utilisateur (sans le mot de passe)
             return res.status(201).json({
