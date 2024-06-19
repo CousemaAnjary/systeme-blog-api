2c8d64bd387fc149b95f73ab02f162a9db83af03
CousemaAnjary <anjaryabdillah@gmail.com>
2024-06-19 23:56:36 +0300

    chore: Mettre à jour le contrôleur DashboardController pour améliorer la gestion des informations utilisateur

--- server/controllers/Api/Backend/DashboardController.js
+++ server/controllers/Api/Backend/DashboardController.js
@@ -3,19 +3,19 @@ const { User } = require('../../../models');
 module.exports = {
     async update(req, res) {
         const userId = req.user.id;
-        const { firstname, lastname, email } = req.body;
+        const { firstname, lastname, email } = req.body
 
         try {
-            const user = await User.findByPk(userId);
+            const user = await User.findByPk(userId)
             if (!user) {
-                return res.status(404).json({ error: 'Utilisateur non trouvé' });
+                return res.status(404).json({ error: 'Utilisateur non trouvé' })
             }
 
-            user.firstname = firstname || user.firstname;
-            user.lastname = lastname || user.lastname;
-            user.email = email || user.email;
+            user.firstname = firstname || user.firstname
+            user.lastname = lastname || user.lastname
+            user.email = email || user.email
 
-            await user.save();
+            await user.save()
 
             return res.status(200).json({
                 user: {
@@ -30,25 +30,25 @@ module.exports = {
                 message: 'Informations utilisateur mises à jour avec succès'
             });
         } catch (error) {
-            console.error('Erreur lors de la mise à jour des informations utilisateur:', error);
+            console.error('Erreur lors de la mise à jour des informations utilisateur:', error)
             return res.status(500).json({ error: 'Erreur interne du serveur' });
         }
     },
 
     async updateProfilePhoto(req, res) {
         const userId = req.user.id;
-        const photoPath = `uploads/${req.file.filename}`;
+        const photoPath = `uploads/${req.file.filename}`
 
         try {
-            const user = await User.findByPk(userId);
+            const user = await User.findByPk(userId)
             if (!user) {
                 return res.status(404).json({ error: 'Utilisateur non trouvé' });
             }
 
-            user.image = photoPath;
+            user.image = photoPath
 
 
-            await user.save();
+            await user.save()
 
             return res.status(200).json({
                 user: {
@@ -58,25 +58,25 @@ module.exports = {
                 message: 'Photo de profil mise à jour avec succès'
             });
         } catch (error) {
-            console.error('Erreur lors de la mise à jour de la photo de profil:', error);
-            return res.status(500).json({ error: 'Erreur interne du serveur' });
+            console.error('Erreur lors de la mise à jour de la photo de profil:', error)
+            return res.status(500).json({ error: 'Erreur interne du serveur' })
         }
     },
 
     // Nouvelle méthode pour mettre à jour la photo de couverture
     updateCoverPhoto: async (req, res) => {
         const userId = req.user.id;
-        const coverPhotoPath = `uploads/${req.file.filename}`;
+        const coverPhotoPath = `uploads/${req.file.filename}`
 
         try {
-            const user = await User.findByPk(userId);
+            const user = await User.findByPk(userId)
             if (!user) {
-                return res.status(404).json({ error: 'Utilisateur non trouvé' });
+                return res.status(404).json({ error: 'Utilisateur non trouvé' })
             }
 
-            user.coverPhoto = coverPhotoPath;
+            user.coverPhoto = coverPhotoPath
 
-            await user.save();
+            await user.save()
 
             return res.status(200).json({
                 user: {
@@ -86,7 +86,7 @@ module.exports = {
                 message: 'Photo de couverture mise à jour avec succès'
             });
         } catch (error) {
-            return res.status(500).json({ error: 'Erreur interne du serveur' });
+            return res.status(500).json({ error: 'Erreur interne du serveur' })
         }
     }
 }
