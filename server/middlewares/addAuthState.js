550366d441776883027cc1f6e767fc4f8b9b37ed
CousemaAnjary <anjaryabdillah@gmail.com>
2024-06-22 18:58:46 +0300

    chore: Update addAuthState middleware to handle authentication state

--- server/middlewares/addAuthState.js
+++ server/middlewares/addAuthState.js
@@ -1,18 +1,18 @@
-const jwt = require('jsonwebtoken');
-const { secretKey } = require('../config'); // Importer la clé secrète depuis config.js
+const jwt = require('jsonwebtoken')
+const { secretKey } = require('../config') // Importer la clé secrète depuis config.js
 
 function addAuthState(req, res, next) {
     const token = req.cookies.authToken; // Lire le cookie authToken
     if (token) {
         try {
-            const decoded = jwt.verify(token, secretKey);
-            res.locals.isLoggedIn = true;
-            res.locals.userId = decoded.userId;
+            const decoded = jwt.verify(token, secretKey)
+            res.locals.isLoggedIn = true
+            res.locals.userId = decoded.userId
         } catch (err) {
-            res.locals.isLoggedIn = false;
+            res.locals.isLoggedIn = false
         }
     } else {
-        res.locals.isLoggedIn = false;
+        res.locals.isLoggedIn = false
     }
     next();
 }
