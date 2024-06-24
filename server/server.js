b53ceba08d00fa9de4b2ece0d0223f065710d9f9
CousemaAnjary <anjaryabdillah@gmail.com>
2024-06-24 20:50:00 +0300

    chore: Update server.js to improve code formatting and remove unnecessary code

--- server/server.js
+++ server/server.js
@@ -1,46 +1,46 @@
-const express = require("express");
-const cors = require('cors');
-const path = require("path");
-const cookieParser = require("cookie-parser"); // Importer cookie-parser
-const addAuthState = require('./middlewares/addAuthState');
-const webRoutes = require("./routes/web");
-const apiRoutes = require("./routes/api");
+const express = require("express")
+const cors = require('cors')
+const path = require("path")
+const cookieParser = require("cookie-parser") // Importer cookie-parser
+const addAuthState = require('./middlewares/addAuthState')
+const webRoutes = require("./routes/web")
+const apiRoutes = require("./routes/api")
 
-const app = express();
+const app = express()
 
 // Configuration de CORS pour permettre les requêtes depuis le frontend
 const corsOptions = {
   origin: 'http://localhost:5173', // URL de votre frontend
-};
-app.use(cors(corsOptions));
+}
+app.use(cors(corsOptions))
 
 // Définir le moteur de vue
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs")
+app.set("views", path.join(__dirname, "views"))
 
 // Middleware pour analyser les requêtes entrantes en JSON et URL-encodées
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 // Utiliser cookie-parser
-app.use(cookieParser());
+app.use(cookieParser())
 
 // Définir les fichiers statiques
-app.use(express.static(path.join(__dirname, "public")));
-app.use('/uploads', express.static(path.join(__dirname, 'uploads'))); // Cette ligne sert le dossier uploads
+app.use(express.static(path.join(__dirname, "public")))
+app.use('/uploads', express.static(path.join(__dirname, 'uploads'))) // Cette ligne sert le dossier uploads
 
 
 // Ajouter le middleware pour injecter l'état de connexion
-app.use(addAuthState);
+app.use(addAuthState)
 
 // Définir les routes
-app.use("/", webRoutes);
-app.use("/api", apiRoutes); // Préfixe toutes les routes API avec /api
+app.use("/", webRoutes)
+app.use("/api", apiRoutes) // Préfixe toutes les routes API avec /api
 
 // Lancement du serveur
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+  console.log(`Server is running on http://localhost:${PORT}`)
+})
 
-module.exports = app;
+module.exports = app
